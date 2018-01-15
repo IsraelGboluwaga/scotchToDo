7464b2c6957a02f9aac57a14cd6b047c5722a1b2
Israel Arunah <israel.arunah@konga.com>
2018-01-15 23:22:28 +0100

    Configured the get, post and delete routes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # scotchToDo
 Simple to-do app following a scotch.io tutorial by Chris Sevilleja - https://scotch.io/tutorials/creating-a-single-page-todo-app-with-node-and-angular
+with some extra arrangements
--- server.js
+++ server.js
@@ -13,5 +13,61 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
+//Defining the model
+var Todo = mongoose.model('Todo', {
+    text: String
+});
+
+
+//app routes
+//Get all todos
+app.get('/api/todos', function (req, res) {
+   Todo.find(function (err, todos) {
+       if (err)
+           res.send(err);
+
+       res.json(todos);
+   });
+});
+
+//Create todo_, send it back after creation
+app.post('/api/todos', function (req, res) {
+
+    // create a todo_, information comes from AJAX request from Angular
+    Todo.create({
+        text: req.body.text,
+        done: false
+    }, function (err, todo) {
+        if (err)
+            res.send(err);
+
+        //Get and return todos after creating another
+
+        Todo.find(function (err, todos) {
+            if (err)
+                res.send(err);
+
+            Todo.json(todos);
+        });
+    })
+});
+
+//Delete a todo_ =>and then return after deleting
+app.delete('/api/todos/:todo_id', function (req, res) {
+    Todo.remove({
+        _id: req.params.todo_id
+    }, function (err, todo) {
+        if (err)
+            res.send(err);
+
+        Todo.find(function (err, todos) {
+            if (err)
+                res.send(err);
+
+            res.json(todos);
+        })
+    });
+});
+
 app.listen('5000');
 console.log('App listening on port 5000');
